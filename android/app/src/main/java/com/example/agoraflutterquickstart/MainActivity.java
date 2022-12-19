50b6a1aef16b43442a851c802726f12bbcbbccbe
Akilan <akilan.a@katomaran.tech>
2022-12-19 18:20:45 +0530

    Agora video calling
     Conflicts:
    	README.md

---
+++ android/app/src/main/java/com/example/agoraflutterquickstart/MainActivity.java
@@ -0,0 +1,7 @@
+package app.agoraVideoCalling;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const appId = 'd8eba56decaf4c809b2258acf4ac7835';
+const token = '007eJxTYFiz+/ybF7KpnCv0LWNUr3J0yc3/sYD7n1v3zFdT8xuc/BgVGFIsUpMSTc1SUpMT00ySLQwsk4yMTC0Sk9NMEpPNLYxNewUWJDcEMjJ8/7aDmZEBAkF8Dob0xMrEkoyiTAYGAAweImw=';
